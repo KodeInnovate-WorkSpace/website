dcf6dd179fdb0faf865f44b500dc418b1be84c85
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-09-20 17:46:56 +0530

    Update:navbar

--- src/components/custom/navbar.jsx
+++ src/components/custom/navbar.jsx
@@ -29,7 +29,10 @@ const Navbar = () => {
             <Link to={"/about"} className="text-gray-600 hover:text-gray-900">
               About
             </Link>
-            <Link to={"/properties"} className="text-gray-600 hover:text-gray-900">
+            <Link
+              to={"/properties"}
+              className="text-gray-600 hover:text-gray-900"
+            >
               Properties
             </Link>
             <Link to={"/contact"} className="text-gray-600 hover:text-gray-900">
@@ -57,9 +60,7 @@ const Navbar = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d={
-                    isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"
-                  }
+                  d={"M4 6h16M4 12h16M4 18h16"}
                 />
               </svg>
             </button>
@@ -69,32 +70,63 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link to={"/"} className="block text-gray-600 hover:text-gray-900">
-              Home
-            </Link>
-            <Link
-              to={"/about"}
-              className="block text-gray-600 hover:text-gray-900"
-            >
-              About
-            </Link>
-            <Link to={"/properties"} className="block text-gray-600 hover:text-gray-900">
-              Properties
-            </Link>
-            <Link
-              to={"/contact"}
-              className="block text-gray-600 hover:text-gray-900"
-            >
-              Contact
-            </Link>
-            <Link
-              to={"/careers"}
-              className="block text-gray-600 hover:text-gray-900"
-            >
-              Careers
-            </Link>
+        <div className="fixed inset-0 bg-black bg-opacity-70 z-50">
+          <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out">
+            <div className="flex justify-between items-center p-4">
+              <h2 className="text-xl text-mainColor font-bold">Deal Done Broker</h2>
+              {/* Close Button */}
+              <button
+                onClick={() => setIsOpen(false)}
+                className="text-gray-600"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <nav className="mt-6 px-4">
+              <Link
+                to={"/"}
+                className="block text-gray-600 hover:text-gray-900 py-2"
+              >
+                Home
+              </Link>
+              <Link
+                to={"/about"}
+                className="block text-gray-600 hover:text-gray-900 py-2"
+              >
+                About
+              </Link>
+              <Link
+                to={"/properties"}
+                className="block text-gray-600 hover:text-gray-900 py-2"
+              >
+                Properties
+              </Link>
+              <Link
+                to={"/contact"}
+                className="block text-gray-600 hover:text-gray-900 py-2"
+              >
+                Contact
+              </Link>
+              <Link
+                to={"/careers"}
+                className="block text-gray-600 hover:text-gray-900 py-2"
+              >
+                Careers
+              </Link>
+            </nav>
           </div>
         </div>
       )}
