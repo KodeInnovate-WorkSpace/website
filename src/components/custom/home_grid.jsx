185a05cc7acb91774406ba842071ed356eaa421f
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-09-26 15:26:19 +0530

    Update: Home UI

--- src/components/custom/home_grid.jsx
+++ src/components/custom/home_grid.jsx
@@ -1,4 +1,5 @@
 import { useCursor } from "@/context/cursor.context";
+import GridCard from "./grid_card";
 
 const HomeGrid = () => {
   const { blendMouseEnter, mouseLeave } = useCursor();
@@ -6,62 +7,36 @@ const HomeGrid = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:px-0">
       {/* grid-1 */}
-      <div
-        onMouseEnter={blendMouseEnter}
-        onMouseLeave={mouseLeave}
-        className="bg-slate-200 h-64 md:h-auto rounded-lg lg:rounded-3xl p-8 flex items-center justify-end"
-      >
-        <h2 className="lg:text-4xl font-bold md:text-3xl">
-          Expert Guidance at Every Turn
-        </h2>
-        <img
-          src="/images/guide.png"
-          alt="trust"
-          loading="lazy"
-          className="w-32 lg:w-full"
-        />
-      </div>
+      <GridCard
+        title={"Client-Focused Services"}
+        image={"/images/focus.png"}
+        gridColor={"mainColor"}
+        textColor={"white"}
+      />
 
       {/* grid-2 */}
-      <div className="bg-mainColor rounded-lg lg:rounded-3xl p-8 flex items-center justify-center text-white  lg:text-3xl font-bold">
-        <img
-          src="/images/property.png"
-          alt="trust"
-          loading="lazy"
-          className="w-32 lg:w-full"
-
-        />
-        <h2>Extensive Property Listings</h2>
-      </div>
+      <GridCard
+        title={"Expert Guidance at Every Turn"}
+        image={"/images/guide.png"}
+        gridColor={"slate-200"}
+        textColor={"black"}
+      />
 
       {/* grid-3 */}
-      <div className="bg-mainColor rounded-lg lg:rounded-3xl p-8 flex items-center justify-center text-white  lg:text-3xl font-bold">
-        <img
-          src="/images/focus.png"
-          alt="trust"
-          loading="lazy"
-          className="w-32 lg:w-full"
-
-        />
-
-        <h2>Client-Focused Services</h2>
-      </div>
+      <GridCard
+        title={"Transparency You Can Trust"}
+        image={"/images/trust.png"}
+        gridColor={"slate-200"}
+        textColor={"black"}
+      />
 
       {/* grid-4 */}
-      <div
-        onMouseEnter={blendMouseEnter}
-        onMouseLeave={mouseLeave}
-        className="bg-slate-200 h-64 md:h-auto rounded-lg lg:rounded-3xl p-8 flex items-center justify-center lg:text-3xl font-bold"
-      >
-        <h2>Transparency You Can Trust</h2>
-        <img
-          src="/images/trust.png"
-          alt="trust"
-          loading="lazy"
-          className="w-32 lg:w-full"
-
-        />
-      </div>
+      <GridCard
+        title={"Extensive Property Listings"}
+        image={"/images/property.png"}
+        gridColor={"mainColor"}
+        textColor={"white"}
+      />
     </div>
   );
 };
