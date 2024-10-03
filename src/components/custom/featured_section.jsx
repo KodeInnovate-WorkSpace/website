15c17ae0a0b44da0d8ba41b53904f9b6b6bc3f8a
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-10-03 14:01:47 +0530

    Update

--- src/components/custom/featured_section.jsx
+++ src/components/custom/featured_section.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import FeaturedCard from './featured_card';
+import React from "react";
+import FeaturedCard from "./featured_card";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const FeaturedSection = () => {
-    return (
-      <div className="container mx-auto flex flex-col items-center justify-center overflow-hidden py-5">
-        <div>
-          <h1 className="text-center font-bold text-gray-800 text-xl lg:text-2xl">
-            Featured Properties
-          </h1>
-          <h3 className="text-center font-semibold text-gray-700 lg:text-xl">
-            To get more information, please click on the images below.
-          </h3>
-        </div>
-          {/* scrolling  */}
-        <div className="my-8 flex">
-
-          <FeaturedCard/>
-        </div>
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center overflow-hidden py-5">
+      <div>
+        <h1 className="text-center font-bold text-gray-800 text-xl lg:text-2xl">
+          Featured Properties
+        </h1>
+        <h3 className="text-center font-semibold text-gray-700 lg:text-xl">
+          To get more information, please click on the images below.
+        </h3>
       </div>
-    );
-  };
 
-export default FeaturedSection
+      {/* scrolling  */}
+      <div className="my-8 flex">
+        <FeaturedCard/>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedSection;
