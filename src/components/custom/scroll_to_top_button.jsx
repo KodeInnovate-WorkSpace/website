d1f80085fe6029d3304988d77fb0ff61171ebc0b
Kaif-Shariff <kaif.shariff1234@gmail.com>
2024-10-03 16:20:29 +0530

    Update z-index of scroll top button

--- src/components/custom/scroll_to_top_button.jsx
+++ src/components/custom/scroll_to_top_button.jsx
@@ -17,7 +17,7 @@ const ScrollToTop = () => {
     <button
       onMouseEnter={blendMouseEnter}
       onMouseLeave={mouseLeave}
-      className="fixed bottom-[65px] right-[20px] lg:right-[30px] w-[56px] h-[56px] bg-secondColor text-white border-none flex items-center justify-center cursor-pointer ease-in hover:bg-mainColor rounded-md"
+      className="fixed bottom-[65px] right-[20px] lg:right-[30px] w-[56px] h-[56px] bg-secondColor text-white border-none flex items-center justify-center cursor-pointer ease-in hover:bg-mainColor rounded-md z-40"
       onClick={scrollToTop}
     >
       <FaArrowUpLong />
